65079b8bad644d613059193f9d5719b22e84f0be
Rahul Lohra <77195427+rahullo@users.noreply.github.com>
2023-03-26 07:44:22 +0530

    Reversing word

--- Arrays/array.py
+++ Arrays/array.py
@@ -1,7 +1,7 @@
 class Solution:
 
     # Maximum and Minimum Element in an Array
-    def findSum(self,A,N): 
+    def findSum(self,A): 
         #code here
         maxi = A[0]
         mini = A[0]
@@ -13,6 +13,18 @@ class Solution:
                 mini = item
         return maxi+mini
     
+    # Reverse an array or string
+    def reverseWord(self,s):
+        # My solution
+        arr = []
+        for i in range(len(s)-1, -1, -1):
+            arr.append(s[i])
+        
+        return ''.join(arr)
+
+        # Other solution(Best)
+        # return s[::-1]
+    
 array = Solution()
 
-print(array.findSum([ 1, 4, 1, 3], 5))
+print(array.reverseWord('Rahul'))
