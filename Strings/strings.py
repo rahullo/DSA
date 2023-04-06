9d2d1035e6bd58d686a0121b28c76472fe8e1d60
Rahul Lohra <77195427+rahullo@users.noreply.github.com>
2023-04-06 08:46:25 +0530

    character Replacement

--- Strings/strings.py
+++ Strings/strings.py
@@ -134,7 +134,24 @@ class String():
 
         return ans
                     
+    def characterReplacement(self, s: str, k: int)-> int:
+        ans = 0
+        maxCount = 0
+        from collections import Counter
+        count = Counter()
+
+        l = 0
+        for r, c in enumerate(s):
+            count[c] += 1
+            maxCount = max(maxCount, count[c])
+            while maxCount + k < r - l + 1:
+                count[s[l]] -= 1
+                l += 1
+            ans = max(ans, r - l + 1)
+
+        return ans
+
 
 
 string = String()
-print(string.lengthOfLongestSubstring("GEEKSFORGEEKS"))
+print(string.characterReplacement("ABAB", 2))
