7ca5095930be1d22a0cd4099eab50ac915aedc5c
Rahul Lohra <77195427+rahullo@users.noreply.github.com>
2023-04-13 09:39:56 +0530

    made changes in top k frequent characters

--- Arrays/array.py
+++ Arrays/array.py
@@ -232,13 +232,20 @@ class Solution:
 
         return ans
     def topKFrequent(self, nums, k):
-        counter = Counter(nums)
-        ans = []
-        print(counter)
-        for key, value in counter.items():
-            ans.append(key)
-            print(key,value)
-        return ans[:k]
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+        for n, c in count.items():
+            freq[c].append(n)
+
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
 
 
 array = Solution()
