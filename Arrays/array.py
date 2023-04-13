bb4607d65e521acfa02dfe32ae3b297a112c1ffd
Rahul Lohra <77195427+rahullo@users.noreply.github.com>
2023-04-13 10:19:37 +0530

    best possible solution for top k frequent

--- Arrays/array.py
+++ Arrays/array.py
@@ -234,7 +234,6 @@ class Solution:
     def topKFrequent(self, nums, k):
         count = {}
         freq = [[] for i in range(len(nums) + 1)]
-
         for n in nums:
             count[n] = 1 + count.get(n, 0)
         
@@ -244,10 +243,10 @@ class Solution:
             freq[c].append(n)
 
         print(freq)
-        
+
         res = []
-        for i in range(len(freq) - 1, 0, -1):
-            for n in freq[i]:
+        for i in range(len(freq) - 1, 0, -1): #   4,  3,   2,   1
+            for n in freq[i]:                   # [], [], [0], [3, 1], []
                 res.append(n)
                 if len(res) == k:
                     return res
