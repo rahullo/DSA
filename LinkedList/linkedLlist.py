4ec4c6b2f2641e5e7f2e0c2eb52d39da1cbb0f69
Rahul Lohra <77195427+rahullo@users.noreply.github.com>
2023-05-03 10:23:35 +0530

    Implementing Linked List

---
+++ LinkedList/linkedLlist.py
@@ -0,0 +1,41 @@
+class Node:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Linked_List:
+    def __init__(self):
+        self.head = None
+        self.tail = self.head
+        self.length = 0
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head == None:
+            self.head = new_node
+            self.tail = new_node
+            self.length = 1
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+            self.length +=1
+
+
+    def printList(self):
+        curr = self.head
+        while curr != None:
+            print(curr.val, end=' ')
+            curr = curr.next
+    
+
+list = Linked_List()
+
+
+arr1 = [4,2,1,3]
+for item in arr1:
+    list.append(item)
+
+list.printList()
+
+
