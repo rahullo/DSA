acd5456446e175ed3bf34af5a7e6248120b270b1
Rahul Lohra <77195427+rahullo@users.noreply.github.com>
2023-05-06 08:30:12 +0530

    trying more optimised appraoch of leetcode 29

--- Arrays/array.py
+++ Arrays/array.py
@@ -334,22 +334,24 @@ class Solution:
         
         # return ans
 
-        if dividend == -2**31 and divisor == -1:
-            return 2**31 - 1
 
-        sign = -1 if (dividend > 0) ^ (divisor > 0) else 1
-        ans = 0
-        dvd = abs(dividend)
-        dvs = abs(divisor)
+        # Optimised approach but not as rule of this question
+        # if dividend == -2**31 and divisor == -1:
+        #     return 2**31 - 1
 
-        while dvd >= dvs:
-            k = 1
-            while k * 2 * dvs <= dvd:
-                k <<= 1
-            dvd -= k * dvs
-            ans += k
+        # sign = -1 if (dividend > 0) ^ (divisor > 0) else 1
+        # ans = 0
+        # dvd = abs(dividend)
+        # dvs = abs(divisor)
 
-        return sign * ans
+        # while dvd >= dvs:
+        #     k = 1
+        #     while k * 2 * dvs <= dvd:
+        #         k <<= 1
+        #     dvd -= k * dvs
+        #     ans += k
+
+        # return sign * ans
 
 
 
