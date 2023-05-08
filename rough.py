3bff3f1909788d3f19edb9a5c8c5a23210b1e15b
Rahul Lohra <77195427+rahullo@users.noreply.github.com>
2023-05-08 09:52:09 +0530

    Integer to Roman 16

--- rough.py
+++ rough.py
@@ -157,4 +157,25 @@ def fnString(arr):
     
     return "".join(ans)
 
-print(fnString(['a', 'b', 'c', 'd', 'e', 'f']))
+# print(fnString(["rahullohra"]))
+
+def intToRoman(num):
+    valueSymbols = [(1000, 'M'), (900, 'CM'),
+                (500, 'D'), (400, 'CD'),
+                (100, 'C'), (90, 'XC'),
+                (50, 'L'), (40, 'XL'),
+                (10, 'X'), (9, 'IX'),
+                (5, 'V'), (4, 'IV'),
+                (1, 'I')]
+    ans = []
+
+    for value, symbol in valueSymbols:
+        if num == 0:
+            break
+        count, num = divmod(num, value)
+        ans.append(symbol * count)
+
+    return ''.join(ans)
+
+
+print(intToRoman(44))
