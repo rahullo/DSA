dadcb08c6f753807eba70baef1e9fa789ddea29c
Rahul Lohra <77195427+rahullo@users.noreply.github.com>
2023-05-08 10:22:26 +0530

    ROman to Integer 13 LeetCode

--- rough.py
+++ rough.py
@@ -186,4 +186,19 @@ def intToRoman(num):
     return ''.join(ans)
 
 
-print(intToRoman(45))
+# print(intToRoman(45))
+
+def romanToInt(s):
+    ans = 0
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+             'C': 100, 'D': 500, 'M': 1000}
+
+    for a, b in zip(s, s[1:]):
+      if roman[a] < roman[b]:
+        ans -= roman[a]
+      else:
+        ans += roman[a]
+
+    return ans + roman[s[-1]]
+
+print(romanToInt('MCMXCIV'))
