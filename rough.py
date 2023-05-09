1de1be0893a723f806a036ecfb8a72cbd88190e4
Rahul Lohra <77195427+rahullo@users.noreply.github.com>
2023-05-09 10:03:30 +0530

    Using recursion

--- rough.py
+++ rough.py
@@ -228,21 +228,39 @@ def letterCombinations1(digits):
 
 
 def letterCombinations(digits):
+    # if not digits:
+    #     return []
+
+    # ans = ['']
+    # digitToLetters = ['', '', 'abc', 'def', 'ghi',
+    #                   'jkl', 'mno', 'pqrs', 'tuv', 'wxyz']
+
+    # for d in digits:
+    #     temp = []
+    #     for s in ans:
+    #         for c in digitToLetters[ord(d) - ord('0')]:
+    #             temp.append(s + c)
+    #     ans = temp
+
+    # return ans
+
     if not digits:
         return []
-
-    ans = ['']
-    digitToLetters = ['', '', 'abc', 'def', 'ghi',
-                      'jkl', 'mno', 'pqrs', 'tuv', 'wxyz']
-
-    for d in digits:
-        temp = []
-        for s in ans:
-            for c in digitToLetters[ord(d) - ord('0')]:
-                temp.append(s + c)
-        ans = temp
-
-    return ans
+        
+    phone = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
+    res = []
+    
+    def backtrack(combination, next_digits):
+        print(combination, next_digits)
+        if not next_digits:
+            res.append(combination)
+            return
+        
+        for letter in phone[next_digits[0]]:
+            backtrack(combination + letter, next_digits[1:])
+    
+    backtrack("", digits)
+    return res
 
 print(letterCombinations("23"))
 print(ord('2')- ord('0'))
