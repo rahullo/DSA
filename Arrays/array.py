b00f855785eace2f77daa0688bbb0a2e2b8891ed
Rahul Lohra <77195427+rahullo@users.noreply.github.com>
2023-05-10 07:59:50 +0530

    spiral matrix 59

--- Arrays/array.py
+++ Arrays/array.py
@@ -409,10 +409,37 @@ class Solution:
                     r -= 1
             print(i, summ)
         return ans
-        
+    
+    # 59 Spiral Matrix 2 (Leetcode)
+    def generateMatrix(self, n):
+        ans = [[0] * n for _ in range(n)]
+        count = 1
+
+        for min in range(n // 2):
+            max = n - min - 1
+            print(min, max)
+            for i in range(min, max):
+                ans[min][i] = count
+                count += 1
+            for i in range(min, max):
+                ans[i][max] = count
+                count += 1
+            for i in range(max, min, -1):
+                ans[max][i] = count
+                count += 1
+            for i in range(max, min, -1):
+                ans[i][min] = count
+                count += 1
+            print(ans)
+        print("hello",n&1)
+
+        if n & 1:
+            ans[n // 2][n // 2] = count
+
+        return ans        
 
 
 array = Solution()
 
-print(array.threeSumClosest([4,0,5,-5,3,3,0,-4,-5], 19))
+print(array.generateMatrix(3))
 
