50ae570a67ca28cb380a6a27cc4d90e40f97c72e
Rahul Lohra <77195427+rahullo@users.noreply.github.com>
2023-05-15 08:51:22 +0530

    1929 other sulution

--- Arrays/array.py
+++ Arrays/array.py
@@ -440,6 +440,12 @@ class Solution:
     def getConcatenation(self, nums):
         return nums + nums
 
+        # Other solution
+        # ans = []
+        # for i in range(2):
+        #     for n in nums:
+        #         ans.append(n)
+        # return ans
 
 array = Solution()
 
