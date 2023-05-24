cb08c26138a81e129ca0c353fe5b5298b4cda180
Rahul Lohra <77195427+rahullo@users.noreply.github.com>
2023-05-24 18:26:45 +0530

    majority Element 169

--- Arrays/array.py
+++ Arrays/array.py
@@ -515,13 +515,24 @@ class Solution:
         return emptySpace == n
 
     def majorityElement(self, nums):
-        res, count = 0, 0
+        # res, count = 0, 0
+
+        # for n in nums:
+        #     if count == 0:
+        #         res = n
+        #     count += (1 if n == res else -1)
+            
+        # return res
+
+        count = {}
+        res, maxCount = 0, 0
 
         for n in nums:
-            if count == 0:
-                res = n
-            count += (1 if n == res else -1)
-            
+            count[n] = 1 + count.get(n, 0)
+            res = n if count[n] > maxCount else res
+
+            maxCount = max(count[n], maxCount)
+
         return res
 
 
