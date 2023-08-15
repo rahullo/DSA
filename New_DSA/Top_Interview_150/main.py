c00d4c8310557a189941b57c24a3e79b215e9c66
Rahul Lohra <77195427+rahullo@users.noreply.github.com>
2023-08-15 19:54:49 +0530

    12. Integer to Roman

--- New_DSA/Top_Interview_150/main.py
+++ New_DSA/Top_Interview_150/main.py
@@ -377,4 +377,29 @@ def romanToInt(s):
 
     return ans + roman[s[-1]]
 
-print(romanToInt("LVIII"))
+# print(romanToInt("LVIII"))
+
+##############################
+# 12. Integer To Roman
+
+def intToRoman(num):
+    valueSymbols = [(1000, 'M'), (900, 'CM'),
+                (500, 'D'), (400, 'CD'),
+                (100, 'C'), (90, 'XC'),
+                (50, 'L'), (40, 'XL'),
+                (10, 'X'), (9, 'IX'),
+                (5, 'V'), (4, 'IV'),
+                (1, 'I')]
+    
+    ans = []
+
+    for value, symbol in valueSymbols:
+        if num == 0:
+            break
+        count, num = divmod(num, value)
+        print(count, num)
+        ans.append(symbol * count)
+
+    return ''.join(ans)
+
+# print(intToRoman(43))
