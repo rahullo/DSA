55c06a060084bf344aea67239652653b17080909
Rahul Lohra <77195427+rahullo@users.noreply.github.com>
2023-08-18 08:30:51 +0530

    15. 3Sum

--- New_DSA/Top_Interview_150/main.py
+++ New_DSA/Top_Interview_150/main.py
@@ -706,4 +706,38 @@ def maxArea(height):
             r-=1
     return ans
 
-print(maxArea([1,8,6,2,5,4,8,3,7]))
+# print(maxArea([1,8,6,2,5,4,8,3,7]))
+
+###############################
+# 15. 3Sum
+from collections import defaultdict
+def threeSum(nums):
+    negative = defaultdict(int)
+    positive = defaultdict(int)
+    zeros = 0
+    for num in nums:
+        if num < 0:
+            negative[num] += 1
+        elif num > 0:
+            positive[num] += 1
+        else:
+            zeros += 1
+    
+    result = []
+    if zeros:
+        for n in negative:
+            if -n in positive:
+                result.append((0, n, -n))       
+        if zeros > 2:
+            result.append((0,0,0))
+
+    for set1, set2 in ((negative, positive), (positive, negative)):
+        set1Items = list(set1.items())
+        for i, (j, k) in enumerate(set1Items):
+            for j2, k2 in set1Items[i:]:
+                if j != j2 or (j == j2 and k > 1):
+                    if -j-j2 in set2:
+                        result.append((j, j2, -j-j2))
+    return result
+
+print(threeSum([-1,0,1,2,-1,-4]))
