5226e6c840786006522f05e799ba16c11843c633
Rahul Lohra <77195427+rahullo@users.noreply.github.com>
2023-08-30 09:30:53 +0530

    48. Rotate image

--- New_DSA/Top_Interview_150/main.py
+++ New_DSA/Top_Interview_150/main.py
@@ -864,3 +864,18 @@ def isValidSudoku(board):
 #                     [".","6",".",".",".",".","2","8","."],
 #                     [".",".",".","4","1","9",".",".","5"],
 #                     [".",".",".",".","8",".",".","7","9"]]))
+
+
+################################
+# 48. Rotate Image
+
+def rotate(matrix):
+    matrix.reverse()
+
+    for i in range(len(matrix)):
+        for j in range(i+1, len(matrix)):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    return matrix
+
+print(rotate([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
