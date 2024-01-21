ef31ac8dab14dc5dd88fcf533737352596e544e8
Rahul Lohra <77195427+rahullo@users.noreply.github.com>
2024-01-21 16:45:11 +0530

    Jai Shree Ram Code

---
+++ New_DSA/jaiShreeRam.py
@@ -0,0 +1,21 @@
+from turtle import *
+title('Jai Shree Ram')
+bgcolor('black')
+pensize(5)
+pencolor("ORANGE")
+Ram_naam = ["जय श्री राम"]*50
+angle = 360/50
+penup()
+sety(-1)
+for _ in range(51):
+    left(angle)
+    forward(260)
+    if Ram_naam:
+        write(Ram_naam.pop(), align='center', font=('Arial', 12, 'normal'))
+    backward(260)
+penup()
+goto(-40, -20)
+pendown()
+write("|| जय श्री राम ||", align='center', font=('Arial', 16, 'bold'))
+hideturtle()
+done()
