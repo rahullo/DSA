2ba2d986e774fefb874751e78e5c8697aaea2340
Rahul Lohra <77195427+rahullo@users.noreply.github.com>
2024-02-14 17:32:36 +0530

    NPTEL

--- New_DSA/rough.py
+++ New_DSA/rough.py
@@ -244,6 +244,35 @@
   
 # print(mystery([22,14,19,65,82,55],1))
 
-triples = [ (x,y,z) for x in range(2,4) for y in range(2,5) for z in range(5,7) if 2*x*y > 3*z ]
+# triples = [ (x,y,z) for x in range(2,4) for y in range(2,5) for z in range(5,7) if 2*x*y > 3*z ]
 
-print(triples)
+# print(triples)
+
+# runs = {
+#     "Test":{
+#         "Rahul":[90,14,35],
+#         "Kohli":[3,103,73,42],
+#         "Pujara":[53,15,133,8]
+#         },
+#     "ODI":{
+#         "Sharma":[37,99],
+#         "Kohli":[63,47]
+#         }
+# }
+
+
+# print(runs)
+
+# # runs["ODI"]["Rahul"].append([74])
+# # runs["ODI"]["Rahul"].extend([74])
+# # runs["ODI"]["Rahul"][0]=74
+# runs["ODI"]["Rahul"]=[74]
+# print(runs)
+
+
+# inventory = {}
+
+# inventory["Amul"] = ["Mystic Mocha",55]
+# inventory["Amul, Mystic Mocha"] = 55
+# inventory[["Amul","Mystic Mocha"]] = 55
+# inventory[("Amul","Mystic Mocha")] = 55
