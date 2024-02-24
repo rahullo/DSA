2e0f0d80346aa68c421fca3ff028b3e4c9c9d381
Rahul Lohra <77195427+rahullo@users.noreply.github.com>
2024-02-24 08:16:33 +0530

    GCD

--- New_DSA/gcd.py
+++ New_DSA/gcd.py
@@ -1,10 +1,10 @@
 def gcd(m, n):
     i=2
     ans = 1;
-    while(i<m and i < n):
+    while(i<=m and i <= n):
         if(m % i==0 and n % i == 0):
             ans = i;
         i+=1
     return ans
 
-print(f"GCD: {gcd(14,63)}")
+print(f"GCD: {gcd(14,28)}")
