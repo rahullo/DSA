b2cc32c621e311b6151524abf66c80d8a1516992
Rahul Lohra <77195427+rahullo@users.noreply.github.com>
2024-03-06 21:24:17 +0530

    immutable and muttable

--- New_DSA/DataStructures/rough.py
+++ New_DSA/DataStructures/rough.py
@@ -9,3 +9,19 @@ string = 'rahul'
 nested = [[2, [37]], 4, ["hello"]]
 
 print(nested[0][1][0])
+
+def randomFunc(arr, index, value):
+    if index >= 0 and index < len(arr):
+        arr[index] = value
+        return True
+    else:
+        value = value+1
+        return False
+    
+arr = [ 1, 3, 5, 6]
+value = 7
+print(arr)
+
+print(randomFunc(arr, 3, value))
+
+print(arr, value)
