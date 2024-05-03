a22d66c4625ef41b0f2f8e16bca480883eb6c31b
Rahul Lohra <77195427+rahullo@users.noreply.github.com>
2024-05-03 08:15:56 +0530

    119. Pascal's Triangle II

--- New_DSA/LeetCode/119_Pascal's_Triangle_II.py
+++ New_DSA/LeetCode/119_Pascal's_Triangle_II.py
@@ -9,7 +9,6 @@ class Solution:
     
     def getRow2(self, rowIndex):
         ans = [1] * (rowIndex+1)
-        print(ans)
         for i in range(2, rowIndex+1):
             for j in range(1, i):
                 ans[i-j] += ans[i-j-1]
