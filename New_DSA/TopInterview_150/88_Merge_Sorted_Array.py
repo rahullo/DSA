85e0000f6d4cf028482a647ef56501ef8c3843bd
Rahul Lohra <77195427+rahullo@users.noreply.github.com>
2024-05-30 07:59:57 +0530

    27. Remove Element

--- New_DSA/TopInterview_150/88_Merge_Sorted_Array.py
+++ New_DSA/TopInterview_150/88_Merge_Sorted_Array.py
@@ -14,6 +14,21 @@ class Solution:
             n-=1
             last-=1
         return nums1
+    
+    def merge2(self, nums1, m, nums2, n):
+        i = m - 1  # nums1's index (the actual nums)
+        j = n - 1  # nums2's index
+        k = m + n - 1  # nums1's index (the next filled position)
+
+        while j >= 0:
+            if i >= 0 and nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                k -= 1
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                k -= 1
+                j -= 1
 
 s = Solution()
 print(s.merge([1, 3, 5, 0, 0, 0, 0], 3, [2, 4, 6, 8], 4))
