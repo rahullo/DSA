28531752cb611c3c7d674b7b2f638c7012197c12
Rahul Lohra <77195427+rahullo@users.noreply.github.com>
2024-08-26 19:19:43 +0530

    generate_k_ary_strings

--- New_DSA/concept/backtracking.py
+++ New_DSA/concept/backtracking.py
@@ -9,4 +9,19 @@ def generate_binary_strings(n, prefix=""):
 
 # Example usage:
 n = 3  # Length of the binary strings
-generate_binary_strings(n)
+# generate_binary_strings(n)
+
+
+def generate_k_ary_strings(n, k, prefix=""):
+    # Base case: If the length of the prefix equals n, print the prefix
+    if n == 0:
+        print(prefix)
+    else:
+        # Recursively add digits from 0 to k-1 to the prefix and reduce the length by 1
+        for i in range(k):
+            generate_k_ary_strings(n - 1, k, prefix + str(i))
+
+# Example usage:
+n = 3  # Length of the strings
+k = 3  # Base (for ternary strings, k=3)
+generate_k_ary_strings(n, k)
