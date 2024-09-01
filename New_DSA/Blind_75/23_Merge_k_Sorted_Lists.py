00858e6a318dc3fdbda8fd1e4259907e50550a0f
Rahul Lohra <77195427+rahullo@users.noreply.github.com>
2024-09-01 14:38:33 +0530

    Merge K sorted lists 23

--- New_DSA/Blind_75/23_Merge_k_Sorted_Lists.py
+++ New_DSA/Blind_75/23_Merge_k_Sorted_Lists.py
@@ -22,6 +22,35 @@ list3.next.next.next = ListNode(12)
 list3.next.next.next.next = ListNode(13)
 list3.next.next.next.next.next = ListNode(14)
 
+class Solution:
+    def mergeKLists(self, lists):
+        if not lists or len(lists) == 0:
+            return None
+
+        while len(lists) > 1:
+            mergedLists = []
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i + 1] if (i + 1) < len(lists) else None
+                mergedLists.append(self.mergeList(l1, l2))
+            lists = mergedLists
+        return lists[0]
+
+    def mergeList(self, l1, l2):
+        dummy = ListNode()
+        tail = dummy
+
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
+        tail.next = l1 or l2
+        
+        return dummy.next
 
 def printList(head):
     if head == None:
@@ -30,26 +59,6 @@ def printList(head):
     print(head.val, end='->')
     printList(head.next)
 
-def mergeKList(lists):
-    node = dummy = ListNode()
-
-    list1 = lists[0]
-    list2 = lists[1]
-    list3 = lists[2]
-    
-    while list1 and list2 and list3:
-        if list1.val < list2.val and list1.val < list3.val:
-            node.next = list1
-            list1 = list1.next
-        elif list2.val < list1.val and list2.val < list3.val:
-            node.next = list2
-            list2 = list2.next
-        else:
-            node.next = list3
-            list3 = list3.next 
-    return dummy.next
-
-
 printList(list1)
 print('\n')
 printList(list2)
@@ -57,4 +66,6 @@ print('\n')
 printList(list3)
 print('\n')
 
-printList(mergeKList([list1, list2, list3]))
+s = Solution()
+
+printList(s.mergeKLists([list1, list2, list3]))
